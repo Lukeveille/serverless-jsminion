b510cbced5ef9321c26e79e3e220c3f8e63b4ebd
Lukeveille <luke@lukeleveille.com>
2020-07-13 09:59:21 -0400

    added form element so enter will submit log in, and added styling changes to submit button when username feild is populated

--- components/Login.js
+++ components/Login.js
@@ -4,7 +4,12 @@ export default ({setUser}) => {
   const [userLogin, setUserLogin] = useState('')
 
   return (
-    <div>
+    <form
+      onSubmit={e => {
+        e.preventDefault;
+        setUser(userLogin);
+      }}
+    >
       <h3>Enter a Username</h3>
       <input
         className="username-input"
@@ -12,9 +17,9 @@ export default ({setUser}) => {
         onChange={e => { setUserLogin(e.target.value) }}
       />
       <div
-        className="center game-button active"
+        className={`center game-button${userLogin? ' outline active' : ''}`}
         onClick={() => { setUser(userLogin) }}
       >Submit</div>
-    </div>
+    </form>
   )
 }
